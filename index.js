7469a167f21269e37fd65706adc7dcd115bf22b8
Elliot Zou <elliotz2@nycstudents.net>
2023-01-16 15:59:09 -0500

    wow

--- index.js
+++ index.js
@@ -1,35 +1,144 @@
-var selectedTiles = [];
 var allTiles = [];
+var selectedTiles = [];
+var gameWon = false;
+var gameStart = false;
 
 function initialize() {
-    //create 16 tiles
-    for(let i = 0; i < 16; i++) {
-        let tile = document.createAttribute("div");
-        tile.setAttribute("id", "tile" + i);
-        tile.classList.add("tile");
-        tile.addEventListener("click", function() {
-            onTileClick(tile);
-        })
+    cutPieces();
+}
+
+function cutPieces() {
+    var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        parts = [],
+        img = new Image();
+    img.onload = split;
+
+    function split() {
+        var origImgWidth = img.width;
+        var origImgHeight = img.height;
+
+        for (var i = 0; i < 16; i++) {
+            var x = (i % 4) * -1 * origImgWidth / 4;
+            var y = Math.floor(i / 4) * -1 * origImgHeight / 4;
+
+            canvas.width = origImgWidth / 4;
+            canvas.height = origImgHeight / 4;
+
+            console.log("x:" + x + " y:" + y + " dWidth:" + origImgWidth + " dHeight:" + origImgHeight);
+
+            ctx.drawImage(this, x, y, origImgWidth, origImgHeight);
+
+            parts.push(canvas.toDataURL());
+
+            /*
+            var slicedImage = document.createElement('img')
+            slicedImage.src = parts[i];
+            var div = document.getElementById('grid');
+            div.appendChild(slicedImage);*/
+
+        }
+
+        console.log(parts);
+        populateGrid(parts);
     }
-    
+
+    img.crossOrigin = 'anonymous';
+    //image src is whatever you want
+    img.src = 'https://external-preview.redd.it/aSQl8u43zYPhUuX1gyVOQpYwVUeE8jFgdJ2rwFdjTgY.png?width=960&crop=smart&auto=webp&v=enabled&s=5997e3a965c12fbf3255c3984d5bc79a2a4a874e'
+    //img.src = 'https://cdn2.unrealengine.com/amoguslandscape-2560x1440-2c59395f3208.jpg'
+    //img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Massey_Ferguson_175_red_tractor_September_2005.jpg/800px-Massey_Ferguson_175_red_tractor_September_2005.jpg';
 }
 
-function displayTiles() {
+function populateGrid(imagePieces) {
+    let positions = [];
+    while (positions.length < 16) {
+        let random = Math.floor(Math.random() * 16) + 1;
+        if (!positions.includes(random)) {
+            positions.push(random);
+        }
+    }
+    /*
+    var slicedImage = document.createElement('img')
+    slicedImage.src = parts[i];
+    var div = document.getElementById('grid');
+    div.appendChild(slicedImage);
+    */
 
-}
+    for (let i = 0; i < 16; i++) {
+        let image = document.createElement('img');
+        image.src = imagePieces[i];
+        image.setAttribute("id", "tile"+i);
+        image.classList.add("tile");
+        image.addEventListener('click', function() {
+            tileClick(image);
+        })
+        allTiles.push(image);
+    }
+    console.log(allTiles);
 
-function onTileClick(tile) {
-
+    for(let i = 0 ; i < 16; i++) {
+        let gridElement = document.getElementById('grid');
+        gridElement.appendChild(allTiles[i]);
+    }
 }
 
 function shuffle() {
+    let positions = [];
+    while (positions.length < 16) {
+        let random = Math.floor(Math.random() * 16) + 1;
+        if (!positions.includes(random)) {
+            positions.push(random);
+        }
+    }
+    for(let i = 0 ; i < 16; i++) {
+        let index = positions[i] - 1;
+        let gridElement = document.getElementById('grid');
+        gridElement.appendChild(allTiles[index]);
+        console.log(index);
+    }
+    gameStart = true;
+}
 
+function tileClick(tile) {
+    if(!gameStart) {
+        return;
+    }
+    if(gameWon) {
+        return;
+    }
+    if(selectedTiles.includes(tile)) {
+        selectedTiles = [];
+        return;
+    }
+    selectedTiles.push(tile);
+    if(selectedTiles.length >= 2) {
+        const grid = document.getElementById('grid');
+        let placeHolder = document.createElement('img');
+        grid.replaceChild(placeHolder, selectedTiles[1]);
+        grid.replaceChild(selectedTiles[1], selectedTiles[0]);
+        grid.replaceChild(selectedTiles[0], placeHolder);
+        selectedTiles=[];
+        checkForWin();
+    }
 }
 
 function checkForWin() {
-
-}
-
-function swap(tile1, tile2) {
-
+    const grid = document.getElementById('grid');
+    let prev = -1;
+    let increasing = true;
+    for (const tile of grid.childNodes) {
+        let number = tile.id.replace(/\D/g,'');
+        if(Number(number) > Number(prev)) {
+            prev = number;
+        }
+        else{
+            increasing = false;
+            break;
+        }
+    }
+    if(increasing) {
+        console.log("win");
+        gameWon = true;
+    }
 }
